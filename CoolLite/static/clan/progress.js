36c9688a0985cb9dfba86f510b703a65559b76f9
sdyxxjj123 <sdyxcdd@126.com>
2021-05-20 14:09:08 +0800

    增加代刀手

--- CoolLite/static/clan/progress.js
+++ CoolLite/static/clan/progress.js
@@ -80,7 +80,13 @@ var vm = new Vue({
             if (cha == undefined) {
                 return '';
             }
-            return `(${cha.cycle}-${cha.boss_num}) <a class="digit${cha.damage.toString().length}">${cha.damage}</a>`;
+            if (cha.behalf == null) {
+                return `(${cha.cycle}-${cha.boss_num}) <a class="digit${cha.damage.toString().length}">${cha.damage}</a>`;
+            }
+            if (cha.behalf == cha.qqid) {
+                return `(${cha.cycle}-${cha.boss_num}) <a class="digit${cha.damage.toString().length}">${cha.damage}</a>`;
+            }
+            return `(${cha.cycle}-${cha.boss_num}) <a class="digit${cha.damage.toString().length}">${cha.damage}<br>由 ${this.find_name(cha.behalf)}(${cha.behalf})上报</a>`;
         },
         cdetail: function (cha) {
             if (cha == undefined) {
